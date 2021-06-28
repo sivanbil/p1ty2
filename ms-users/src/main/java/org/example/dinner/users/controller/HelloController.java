1c04854b51817517417c6877a85e0480fbe19cd9
sivan <sivan@sivandeMacBook-Air.local>
2021-06-28 21:50:56 +0800

    init ms

---
+++ ms-users/src/main/java/org/example/dinner/users/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.example.dinner.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello(String name) {
+        return "name = " + name;
+    }
+}
